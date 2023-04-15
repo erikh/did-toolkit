e429d0d75fedbc3c0d5bd83580b3ba9f005fe948
Erik Hollensbe <git@hollensbe.org>
2023-04-15 13:54:58 -0700

    URL generation as well
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/url.rs
+++ src/url.rs
@@ -4,23 +4,97 @@ use time::{
     format_description::FormatItem, macros::format_description, OffsetDateTime, PrimitiveDateTime,
 };
 
-use crate::string::url_decoded;
+use crate::string::{url_decoded, url_encoded};
 
 static VERSION_TIME_FORMAT: &'static [FormatItem<'static>] =
     format_description!("[year]-[month]-[day]T[hour]:[minute]:[second]");
 
 #[derive(Default, Debug, PartialEq)]
 pub struct URL {
-    pub method: Vec<u8>,
     pub name: Vec<u8>,
+    pub method: Vec<u8>,
     pub path: Option<Vec<u8>>,
     pub fragment: Option<Vec<u8>>,
     pub service: Option<String>,
-    pub relative_ref: Option<String>,
+    pub relative_ref: Option<Vec<u8>>,
     pub version_id: Option<String>,
     pub version_time: Option<OffsetDateTime>,
     pub hash_link: Option<String>,
-    pub extra_query: Option<BTreeMap<String, String>>,
+    pub extra_query: Option<BTreeMap<Vec<u8>, Vec<u8>>>,
+}
+
+impl ToString for URL {
+    fn to_string(&self) -> String {
+        let mut ret = String::from("did:");
+
+        ret += &url_encoded(&self.name);
+        ret += &(":".to_string() + &url_encoded(&self.method));
+
+        if let Some(path) = &self.path {
+            ret += &("/".to_string() + &url_encoded(&path));
+        }
+
+        if self.service.is_some()
+            || self.relative_ref.is_some()
+            || self.version_id.is_some()
+            || self.version_time.is_some()
+            || self.hash_link.is_some()
+            || self.extra_query.is_some()
+        {
+            ret += "?";
+
+            if let Some(service) = &self.service {
+                ret += &("service=".to_string() + &service);
+                ret += "&";
+            }
+
+            if let Some(relative_ref) = &self.relative_ref {
+                ret += &("relativeRef=".to_string() + &url_encoded(&relative_ref));
+                ret += "&";
+            }
+
+            if let Some(version_id) = &self.version_id {
+                ret += &("versionId=".to_string() + &version_id);
+                ret += "&";
+            }
+
+            if let Some(version_time) = self.version_time {
+                ret += &("versionTime=".to_string()
+                    + &format!(
+                        "{}-{:02}-{:02}T{:02}:{:02}:{:02}",
+                        version_time.year(),
+                        u8::from(version_time.month()),
+                        version_time.day(),
+                        version_time.hour(),
+                        version_time.minute(),
+                        version_time.second()
+                    ));
+                ret += "&";
+            }
+
+            if let Some(hash_link) = &self.hash_link {
+                ret += &("hl=".to_string() + &hash_link);
+                ret += "&";
+            }
+
+            if let Some(extra_query) = &self.extra_query {
+                for (key, value) in extra_query.iter() {
+                    ret += &format!("{}={}&", url_encoded(key), url_encoded(value));
+                }
+            }
+
+            ret = match ret.strip_suffix("&") {
+                Some(ret) => ret.to_string(),
+                None => ret,
+            };
+        }
+
+        if let Some(fragment) = &self.fragment {
+            ret += &("#".to_string() + &url_encoded(&fragment));
+        }
+
+        ret
+    }
 }
 
 impl URL {
@@ -122,12 +196,12 @@ impl URL {
         &mut self,
         left: &str,
         right: &str,
-        extra_query: &mut BTreeMap<String, String>,
+        extra_query: &mut BTreeMap<Vec<u8>, Vec<u8>>,
     ) -> Result<(), anyhow::Error> {
         match left {
             "service" => self.service = Some(right.to_string()),
             "relativeRef" => {
-                self.relative_ref = Some(String::from_utf8_lossy(&url_decoded(right)).to_string())
+                self.relative_ref = Some(url_decoded(right));
             }
             "versionId" => self.version_id = Some(right.to_string()),
             "versionTime" => {
@@ -136,7 +210,7 @@ impl URL {
             }
             "hl" => self.hash_link = Some(right.to_string()),
             _ => {
-                extra_query.insert(left.to_string(), right.to_string());
+                extra_query.insert(url_decoded(left), url_decoded(right));
             }
         }
 
@@ -153,7 +227,7 @@ impl URL {
                     self.match_fixed_query_params(left, right, &mut extra_query)?;
                 }
                 None => {
-                    extra_query.insert(query.to_string(), Default::default());
+                    extra_query.insert(url_decoded(query), Default::default());
                 }
             }
         } else {
@@ -163,7 +237,7 @@ impl URL {
                         self.match_fixed_query_params(left, right, &mut extra_query)?;
                     }
                     None => {
-                        extra_query.insert(part.to_string(), Default::default());
+                        extra_query.insert(url_decoded(part), Default::default());
                     }
                 }
             }
@@ -178,6 +252,201 @@ impl URL {
 }
 
 mod tests {
+    #[test]
+    fn test_to_string() {
+        use super::URL;
+        use std::collections::BTreeMap;
+        use time::OffsetDateTime;
+
+        let url = URL {
+            name: "abcdef".into(),
+            method: "123456".into(),
+            ..Default::default()
+        };
+
+        assert_eq!(url.to_string(), "did:abcdef:123456");
+
+        let url = URL {
+            name: "abcdef".into(),
+            method: "123456".into(),
+            path: Some("path".into()),
+            ..Default::default()
+        };
+
+        assert_eq!(url.to_string(), "did:abcdef:123456/path");
+
+        let url = URL {
+            name: "abcdef".into(),
+            method: "123456".into(),
+            fragment: Some("fragment".into()),
+            ..Default::default()
+        };
+
+        assert_eq!(url.to_string(), "did:abcdef:123456#fragment");
+
+        let url = URL {
+            name: "abcdef".into(),
+            method: "123456".into(),
+            path: Some("path".into()),
+            fragment: Some("fragment".into()),
+            ..Default::default()
+        };
+
+        assert_eq!(url.to_string(), "did:abcdef:123456/path#fragment");
+
+        let url = URL {
+            name: "abcdef".into(),
+            method: "123456".into(),
+            service: Some("frobnik".into()),
+            ..Default::default()
+        };
+
+        assert_eq!(url.to_string(), "did:abcdef:123456?service=frobnik");
+
+        let url = URL {
+            name: "abcdef".into(),
+            method: "123456".into(),
+            service: Some("frobnik".into()),
+            relative_ref: Some("/ref".into()),
+            ..Default::default()
+        };
+
+        assert_eq!(
+            url.to_string(),
+            "did:abcdef:123456?service=frobnik&relativeRef=%2Fref"
+        );
+
+        let url = URL {
+            name: "abcdef".into(),
+            method: "123456".into(),
+            service: Some("frobnik".into()),
+            relative_ref: Some("/ref".into()),
+            version_id: Some("1".into()),
+            ..Default::default()
+        };
+
+        assert_eq!(
+            url.to_string(),
+            "did:abcdef:123456?service=frobnik&relativeRef=%2Fref&versionId=1"
+        );
+
+        let url = URL {
+            name: "abcdef".into(),
+            method: "123456".into(),
+            service: Some("frobnik".into()),
+            relative_ref: Some("/ref".into()),
+            version_id: Some("1".into()),
+            hash_link: Some("myhash".into()),
+            ..Default::default()
+        };
+
+        assert_eq!(
+            url.to_string(),
+            "did:abcdef:123456?service=frobnik&relativeRef=%2Fref&versionId=1&hl=myhash",
+        );
+
+        let mut map = BTreeMap::new();
+        map.insert("extra".into(), "parameter".into());
+
+        let url = URL {
+            name: "abcdef".into(),
+            method: "123456".into(),
+            service: Some("frobnik".into()),
+            relative_ref: Some("/ref".into()),
+            version_id: Some("1".into()),
+            hash_link: Some("myhash".into()),
+            extra_query: Some(map),
+            ..Default::default()
+        };
+
+        assert_eq!(
+            url.to_string(), 
+            "did:abcdef:123456?service=frobnik&relativeRef=%2Fref&versionId=1&hl=myhash&extra=parameter",
+        );
+
+        let mut map = BTreeMap::new();
+        map.insert("extra".into(), "".into());
+
+        let url = URL {
+                name: "abcdef".into(),
+                method: "123456".into(),
+                service: Some("frobnik".into()),
+                relative_ref: Some("/ref".into()),
+                version_id: Some("1".into()),
+                hash_link: Some("myhash".into()),
+                extra_query: Some(map),
+                ..Default::default()
+            };
+
+        assert_eq!(
+            url.to_string(),
+            "did:abcdef:123456?service=frobnik&relativeRef=%2Fref&versionId=1&hl=myhash&extra=",
+        );
+
+        let mut map = BTreeMap::new();
+        map.insert("extra".into(), "parameter".into());
+
+        let url = URL {
+                name: "abcdef".into(),
+                method: "123456".into(),
+                extra_query: Some(map),
+                ..Default::default()
+            };
+
+        assert_eq!(
+            url.to_string(),
+            "did:abcdef:123456?extra=parameter",
+        );
+
+        let mut map = BTreeMap::new();
+        map.insert("extra".into(), "".into());
+
+        let url = URL {
+            name: "abcdef".into(),
+            method: "123456".into(),
+            extra_query: Some(map),
+            ..Default::default()
+        };
+
+        assert_eq!(
+            url.to_string(),
+            "did:abcdef:123456?extra=",
+        );
+
+        let url = URL {
+            name: "abcdef".into(),
+            method: "123456".into(),
+            path: Some("path".into()),
+            fragment: Some("fragment".into()),
+            service: Some("frobnik".into()),
+            relative_ref: Some("/ref".into()),
+            version_id: Some("1".into()),
+            hash_link: Some("myhash".into()),
+            ..Default::default()
+        };
+
+        assert_eq!(
+            url.to_string(),
+            "did:abcdef:123456/path?service=frobnik&relativeRef=%2Fref&versionId=1&hl=myhash#fragment",
+        );
+
+        let url = URL {
+            name: "abcdef".into(),
+            method: "123456".into(),
+            path: Some("path".into()),
+            fragment: Some("fragment".into()),
+            service: Some("frobnik".into()),
+            relative_ref: Some("/ref".into()),
+            version_id: Some("1".into()),
+            version_time: Some(OffsetDateTime::from_unix_timestamp(260690400).unwrap()),
+            ..Default::default()
+        };
+
+        assert_eq!(
+            url.to_string(),
+            "did:abcdef:123456/path?service=frobnik&relativeRef=%2Fref&versionId=1&versionTime=1978-04-06T06:00:00#fragment",
+        );
+    }
 
     #[test]
     fn test_parse() {
@@ -360,24 +629,6 @@ mod tests {
                 name: "abcdef".into(),
                 method: "123456".into(),
                 extra_query: Some(map),
-                ..Default::default()
-            }
-        );
-
-        let url = URL::parse(
-            "did:abcdef:123456/path?service=frobnik&relativeRef=%2Fref&versionId=1&hl=myhash",
-        )
-        .unwrap();
-        assert_eq!(
-            url,
-            URL {
-                name: "abcdef".into(),
-                method: "123456".into(),
-                path: Some("path".into()),
-                service: Some("frobnik".into()),
-                relative_ref: Some("/ref".into()),
-                version_id: Some("1".into()),
-                hash_link: Some("myhash".into()),
                 ..Default::default()
             }
         );
