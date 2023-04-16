4312b215cbf18a39877bf544adeba14553f93652
Erik Hollensbe <git@hollensbe.org>
2023-04-16 15:04:20 -0700

    Verification Method Types
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/document.rs
+++ src/document.rs
@@ -4,10 +4,38 @@ use serde::{Deserialize, Serialize};
 use url::Url;
 
 #[derive(Hash, PartialEq, PartialOrd, Eq, Serialize, Deserialize)]
+pub enum VerificationMethodType {
+    JWK2020,
+    ECDSASECP256K12019,
+    Ed255192018,
+    Bls12381G12020,
+    Bls12381G22020,
+    PGP2021,
+    ECDSASECP256K1Recovery2020,
+    VerifiableCondition2021,
+}
+
+impl ToString for VerificationMethodType {
+    fn to_string(&self) -> String {
+        match self {
+            Self::JWK2020 => "JsonWebKey2020",
+            Self::ECDSASECP256K12019 => "EcdsaSecp256k1VerificationKey2019",
+            Self::Ed255192018 => "Ed25519VerificationKey2018",
+            Self::Bls12381G12020 => "Bls12381G1Key2020",
+            Self::Bls12381G22020 => "Bls12381G2Key2020",
+            Self::PGP2021 => "PgpVerificationKey2021",
+            Self::ECDSASECP256K1Recovery2020 => "EcdsaSecp256k1RecoveryMethod2020",
+            Self::VerifiableCondition2021 => "VerifiableCondition2021",
+        }
+        .to_string()
+    }
+}
+
+#[derive(Hash, PartialEq, PartialOrd, Eq, Serialize, Deserialize)]
 pub struct VerificationMethod {
     id: URL,
     controller: DID,
-    typ: String,
+    typ: VerificationMethodType,
     public_key_jwk: Option<JWK>,
     public_key_multibase: Option<MultiBase>,
 }
@@ -23,9 +51,9 @@ pub struct ServiceEndpoint {
 #[derive(Hash, PartialEq, PartialOrd, Eq, Serialize, Deserialize)]
 pub struct Document {
     id: DID,
-    also_known_as: Option<HashSafeHashSet<String>>,
+    also_known_as: Option<HashSafeHashSet<Url>>,
     controller: Option<Either<DID, HashSafeHashSet<DID>>>,
-    verification_method: Option<HashSafeHashSet<VerificationMethod>>,
+    verification_method: Option<Vec<VerificationMethod>>,
     authentication: Option<Either<HashSafeHashSet<VerificationMethod>, URL>>,
     assertion_method: Option<Either<HashSafeHashSet<VerificationMethod>, URL>>,
     key_agreeement: Option<Either<HashSafeHashSet<VerificationMethod>, URL>>,
